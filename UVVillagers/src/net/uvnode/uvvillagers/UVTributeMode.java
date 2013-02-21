ef0dbd630f31147b933b75a50e7edf7cca7bd61d
jcornwellshiel <jcornwellshiel@gmail.com>
2013-02-20 21:56:58 -0500

    Converted hardcoded constants to config-loaded variables.

---
+++ UVVillagers/src/net/uvnode/uvvillagers/UVTributeMode.java
@@ -0,0 +1,3 @@
+package net.uvnode.uvvillagers;
+
+public enum UVTributeMode { PLAYER, CHEST }
--- UVVillagers/src/net/uvnode/uvvillagers/UVVillagers.java
+++ UVVillagers/src/net/uvnode/uvvillagers/UVVillagers.java
@@ -24,31 +24,38 @@ import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
-
 /**
  * @author James Cornwell-Shiel
  *
+ * Adds village tributes and additional zombie siege functionality.
+ * 
  */
 public final class UVVillagers extends JavaPlugin implements Listener {
 	
+	// Initialize null active siege
 	UVSiege activeSiege = null;
+	
+	// Configuration Settings
+	UVTributeMode tributeMode;
+
+	int tributeRange, villagerCount, minPerVillagerCount, maxPerVillagerCount, baseSiegeBonus, minPerSiegeKill, maxPerSiegeKill; 
+
 	
 	@Override
 	public void onEnable() {
 		getServer().getPluginManager().registerEvents(this, this);
 		
-		//Start timer
+		loadConfig();
+		
+		// Step through worlds every 20 ticks and throw UVTimeEvents for various times of day.
 		getServer().getScheduler().runTaskTimerAsynchronously(this, new Runnable() {
-
 			@Override
 			public void run() {
-				// TODO Auto-generated method stub
 				List<World> worlds = getServer().getWorlds();
 				for (int i = 0; i < worlds.size(); i++) {
 					if (worlds.get(i).getTime() >= 0 && worlds.get(i).getTime() < 20) {
 						UVTimeEvent event = new UVTimeEvent(worlds.get(i), UVTimeEventType.DAWN);
 						getServer().getPluginManager().callEvent(event);
-						//getServer().broadcastMessage(event.getMessage());
 					}
 				}
 			}
@@ -56,6 +63,20 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 		}, 0, 20);
 	}
 	
+	
+	
+	private void loadConfig() {
+		tributeRange = getConfig().getInt("tributeRange", 64);
+		villagerCount = getConfig().getInt("villagerCount", 20);
+		minPerVillagerCount = getConfig().getInt("minPerVillagerCount", 0);
+		maxPerVillagerCount = getConfig().getInt("maxPerVillagerCount", 3);
+		baseSiegeBonus = getConfig().getInt("baseSiegeBonus", 1);
+		minPerSiegeKill = getConfig().getInt("minPerSiegeKill", 1);
+		maxPerSiegeKill = getConfig().getInt("maxPerSiegeKill", 2);
+	}
+
+
+
 	@Override
 	public void onDisable() {
 		
@@ -74,17 +95,24 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 		return false; 
 	}
 
+	/**
+	 * @param event
+	 * 
+	 * Event handler for creatures spawning.
+	 */
 	@EventHandler
 	public void creatureSpawn(CreatureSpawnEvent event) {
 		int x, y, z;
 		switch(event.getSpawnReason()) {
 			case VILLAGE_DEFENSE:
+				// Generally speaking I think this one is always Iron Golems.
 				x = event.getEntity().getLocation().getBlockX();
 				y = event.getEntity().getLocation().getBlockY();
 				z = event.getEntity().getLocation().getBlockZ();
 				getLogger().info("CreatureSpawnEvent " + event.getEntityType().getName() + " VILLAGE_DEFENSE @ " + x + "," + y + "," + z + "!");
 				break;
 			case VILLAGE_INVASION:
+				// Zombie siege?! Oh snap!
 				x = event.getEntity().getLocation().getBlockX();
 				y = event.getEntity().getLocation().getBlockY();
 				z = event.getEntity().getLocation().getBlockZ();
@@ -115,51 +143,51 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 			case DAWN:
 				Collection<Villager> villagers = event.getWorld().getEntitiesByClass(org.bukkit.entity.Villager.class);
 				List<Player> players = event.getWorld().getPlayers();
-				Map<String, Integer> playerRewards = new HashMap<String, Integer>();
+				Map<String, Integer> playerVillagerCounts = new HashMap<String, Integer>();
 				Iterator<Villager> villagerIterator = villagers.iterator();
 				while (villagerIterator.hasNext()) {
 					Villager v = villagerIterator.next();
 					for (int i = 0; i < players.size(); i++) {
-						if (players.get(i).getLocation().distanceSquared(v.getLocation()) < 4096.0) {
-							if (playerRewards.get(players.get(i).getName()) != null) {
-								playerRewards.put(players.get(i).getName(), playerRewards.get(players.get(i).getName()) + 1);
+						if (players.get(i).getLocation().distanceSquared(v.getLocation()) < tributeRange * tributeRange) {
+							if (playerVillagerCounts.get(players.get(i).getName()) != null) {
+								playerVillagerCounts.put(players.get(i).getName(), playerVillagerCounts.get(players.get(i).getName()) + 1);
 							} else {
-								playerRewards.put(players.get(i).getName(), 1);
+								playerVillagerCounts.put(players.get(i).getName(), 1);
 							}
 						}
 					}
 				}
 				
-				Iterator<String> prs = playerRewards.keySet().iterator();
+				Iterator<String> prs = playerVillagerCounts.keySet().iterator();
 				while (prs.hasNext()) {
 					String pname = prs.next();
-					int numNPCs = playerRewards.get(pname);
-					int amount = 0;
-					getLogger().info(pname + " is close to " + numNPCs + " villagers.");
+					int numVillagers = playerVillagerCounts.get(pname);
+					int tributeAmount = 0;
+					getLogger().info(pname + " is close to " + numVillagers + " villagers.");
 					Random rng = new Random();
 					if (activeSiege != null) {
 						int kills = activeSiege.getPlayerKills(pname);
 						for (int i = 0; i < kills; i++) {
-							amount += rng.nextInt(2);
+							tributeAmount += rng.nextInt(maxPerSiegeKill + 1) + (maxPerSiegeKill - minPerSiegeKill);
 						}
 					}
-					if (numNPCs > 20) {
-						for (int i = 0; i < (int)(numNPCs / 20); i++) {
+					if (numVillagers >= villagerCount) {
+						for (int i = 0; i < (int)(numVillagers / villagerCount); i++) {
 							if (activeSiege == null) {
-								amount += rng.nextInt(3);
+								tributeAmount += rng.nextInt(maxPerVillagerCount + 1) + (maxPerVillagerCount - minPerVillagerCount);
 							} else {
-								amount += rng.nextInt(3) + 1;								
+								tributeAmount += rng.nextInt(maxPerVillagerCount + 1) + (maxPerVillagerCount - minPerVillagerCount) + baseSiegeBonus;
 							}
 						}
-						if (amount > 0) {
-							ItemStack items = new ItemStack(Material.EMERALD, amount);
+						if (tributeAmount > 0) {
+							ItemStack items = new ItemStack(Material.EMERALD, tributeAmount);
 							getServer().getPlayer(pname).getInventory().addItem(items);
-							getServer().getPlayer(pname).sendMessage("Grateful villagers gave you " + amount + " emeralds!");
+							getServer().getPlayer(pname).sendMessage("Grateful villagers gave you " + tributeAmount + " emeralds!");
 						}
 						else
 							getServer().getPlayer(pname).sendMessage("The villagers didn't have any emeralds for you today.");
 
-						getLogger().info(pname + " received " + amount + " emeralds.");						
+						getLogger().info(pname + " received " + tributeAmount + " emeralds.");						
 					}
 				}
 				activeSiege = null;
