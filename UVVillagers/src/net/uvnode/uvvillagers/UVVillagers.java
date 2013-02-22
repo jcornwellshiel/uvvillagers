f87021af2216333f571c2da2d225094693be072b
jcornwellshiel <jcornwellshiel@gmail.com>
2013-02-22 00:33:53 -0500

    Reworked code to use Village object for population and distance. Added random additional mob spawns for sieges.

--- UVVillagers/src/net/uvnode/uvvillagers/UVVillagers.java
+++ UVVillagers/src/net/uvnode/uvvillagers/UVVillagers.java
@@ -3,27 +3,33 @@
  */
 package net.uvnode.uvvillagers;
 
-import java.util.Collection;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
 
+import net.minecraft.server.v1_4_R1.Village;
+import net.minecraft.server.v1_4_R1.VillageSiege;
+import net.minecraft.server.v1_4_R1.WorldServer;
+
+import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.LivingEntity;
 import org.bukkit.entity.Player;
-import org.bukkit.entity.Villager;
 import org.bukkit.event.Listener;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.entity.CreatureSpawnEvent;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.inventory.ItemStack;
 
+import org.bukkit.craftbukkit.v1_4_R1.CraftWorld;
 /**
  * @author James Cornwell-Shiel
  *
@@ -32,17 +38,20 @@ import org.bukkit.inventory.ItemStack;
  */
 public final class UVVillagers extends JavaPlugin implements Listener {
 	
+	WorldServer _worldserver;
 	// Initialize null active siege
 	UVSiege activeSiege = null;
 	
 	// Configuration Settings
 	UVTributeMode tributeMode;
 
-	int tributeRange, villagerCount, minPerVillagerCount, maxPerVillagerCount, baseSiegeBonus, minPerSiegeKill, maxPerSiegeKill; 
+	int tributeRange, villagerCount, minPerVillagerCount, maxPerVillagerCount, baseSiegeBonus, minPerSiegeKill, maxPerSiegeKill, chanceOfExtraZombies; 
 
 	
 	@Override
 	public void onEnable() {
+		_worldserver = ((CraftWorld) getServer().getWorlds().get(0)).getHandle();
+
 		getServer().getPluginManager().registerEvents(this, this);
 
 		saveDefaultConfig();
@@ -74,6 +83,8 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 		baseSiegeBonus = getConfig().getInt("baseSiegeBonus", 1);
 		minPerSiegeKill = getConfig().getInt("minPerSiegeKill", 1);
 		maxPerSiegeKill = getConfig().getInt("maxPerSiegeKill", 2);
+		chanceOfExtraZombies = getConfig().getInt("chanceOfExtraZombies", 20);
+		if (chanceOfExtraZombies > 50) chanceOfExtraZombies = 50; // Cap this so that there are no infinite zombie spawns.
 		getLogger().info("Configuration loaded.");
 	}
 
@@ -87,6 +98,26 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
 		if(cmd.getName().equalsIgnoreCase("uvv")){
 			sender.sendMessage("- UVVillagers -");
+			@SuppressWarnings("unchecked")
+			List<Village> villages = _worldserver.villages.getVillages();
+			sender.sendMessage(villages.size() + " villages are loaded.");
+			if (sender instanceof Player) {
+				Location pLoc = ((Player)sender).getLocation();
+				
+				
+				Village closest = _worldserver.villages.getClosestVillage(pLoc.getBlockX(), pLoc.getBlockY(), pLoc.getBlockZ(), 256);
+				sender.sendMessage("Closest village at " + closest.getCenter().x + ", " + closest.getCenter().y + ", " + closest.getCenter().z);
+				sender.sendMessage(" - Population: " + closest.getPopulationCount());
+				sender.sendMessage(" - Size: " + closest.getSize());
+				sender.sendMessage(" - Doors: " + closest.getDoorCount());
+				getLogger().info(closest.d()+"");
+				getLogger().info(closest.toString());
+				getLogger().info("a: " + closest.a(((Player)sender).getName()));
+				getLogger().info("d: " + closest.d(((Player)sender).getName()));
+				//new VillageSiege(_worldserver);
+			}
+			
+/*
 			if (args.length > 0 && args[0] == "siege") {
 				if (activeSiege == null) {
 					sender.sendMessage("No sieges.");
@@ -98,10 +129,10 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 				loadConfig();
 			} else {
 				sender.sendMessage("Try \"/uvv siege\" for current siege info, or \"/uvv reload\" to reload the config.");
-			}
+			}*/
 			return true;
 		}
-		return false; 
+		return false;
 	}
 
 	/**
@@ -113,24 +144,26 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 	public void creatureSpawn(CreatureSpawnEvent event) {
 		int x, y, z;
 		switch(event.getSpawnReason()) {
-			case VILLAGE_DEFENSE:
-				// Generally speaking I think this one is always Iron Golems.
-				x = event.getEntity().getLocation().getBlockX();
-				y = event.getEntity().getLocation().getBlockY();
-				z = event.getEntity().getLocation().getBlockZ();
-				getLogger().info("CreatureSpawnEvent " + event.getEntityType().getName() + " VILLAGE_DEFENSE @ " + x + "," + y + "," + z + "!");
-				break;
 			case VILLAGE_INVASION:
 				// Zombie siege?! Oh snap!
 				x = event.getEntity().getLocation().getBlockX();
 				y = event.getEntity().getLocation().getBlockY();
 				z = event.getEntity().getLocation().getBlockZ();
+				
 				getLogger().info("CreatureSpawnEvent " + event.getEntityType().getName() + " VILLAGE_INVASION @ " + x + "," + y + "," + z + "!");
 				if (activeSiege == null) {
 					getServer().broadcastMessage("A zombie siege has begun!!!");
 					activeSiege = new UVSiege(event.getEntity().getWorld().getTime());
 				}
 				activeSiege.addSpawn(event.getEntity());
+				if (chanceOfExtraZombies > 1) { 
+					Random rng = new Random();
+					if (chanceOfExtraZombies > rng.nextInt(100)) {
+						getLogger().info("An extra zombie spawns!");
+						activeSiege.addSpawn((LivingEntity)event.getEntity().getWorld().spawnEntity(event.getEntity().getLocation(), EntityType.ZOMBIE));
+					}
+					
+				}
 				break;
 			default:
 				break;
@@ -141,7 +174,8 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 	public void entityDeath(EntityDeathEvent event) {
 		if (activeSiege != null) {
 			if (activeSiege.checkEntityId(event.getEntity().getEntityId())) {
-				activeSiege.addPlayerKill(event.getEntity().getKiller().getName());
+				if(event.getEntity().getKiller() != null)
+					activeSiege.addPlayerKill(event.getEntity().getKiller().getName());
 			}
 		}
 	}
@@ -150,20 +184,16 @@ public final class UVVillagers extends JavaPlugin implements Listener {
 	public void emeraldsAtDawn(UVTimeEvent event) {
 		switch(event.getType()) {
 			case DAWN:
-				Collection<Villager> villagers = event.getWorld().getEntitiesByClass(org.bukkit.entity.Villager.class);
-				List<Player> players = event.getWorld().getPlayers();
 				Map<String, Integer> playerVillagerCounts = new HashMap<String, Integer>();
-				Iterator<Villager> villagerIterator = villagers.iterator();
-				while (villagerIterator.hasNext()) {
-					Villager v = villagerIterator.next();
-					for (int i = 0; i < players.size(); i++) {
-						if (players.get(i).getLocation().distanceSquared(v.getLocation()) < tributeRange * tributeRange) {
-							if (playerVillagerCounts.get(players.get(i).getName()) != null) {
-								playerVillagerCounts.put(players.get(i).getName(), playerVillagerCounts.get(players.get(i).getName()) + 1);
-							} else {
-								playerVillagerCounts.put(players.get(i).getName(), 1);
-							}
-						}
+				List<Player> players = event.getWorld().getPlayers();
+				for (int i = 0; i < players.size(); i++) {
+					Village closest = _worldserver.villages.getClosestVillage(
+							players.get(i).getLocation().getBlockX(), 
+							players.get(i).getLocation().getBlockY(), 
+							players.get(i).getLocation().getBlockZ(), 
+							tributeRange);
+					if (closest != null) {
+						playerVillagerCounts.put(players.get(i).getName(), closest.getPopulationCount());
 					}
 				}
 				
